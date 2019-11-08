ef15cd33d5808a613dbf75dd75ea6e7a94c68f47
Lisandro Gomez <draivenl@gmail.com>
2019-11-07 22:32:19 -0500

    FlatList with renderItem and ItemSeparatorComponent

--- src/videos/containers/SuggestionList.js
+++ src/videos/containers/SuggestionList.js
@@ -1,31 +1,41 @@
 import React, {Component} from "react";
 import { View, Text, FlatList } from "react-native";
 import Layout from "../components/SuggestionListLayout";
+import Empty from "../components/empty";
+import VerticalSeparator from "../components/vertical-separator";
 
 class SuggestionList extends Component {
+    renderEmpty = () => {
+        return <Empty text='No hay componentes en la lista'/>
+    }
+    itemSeparator = () => {
+        return <VerticalSeparator text='No hay componentes en la lista'/>
+    }
     render(){
         const list = [
             {   
                 key: '1',
-                name: 'Lisandro',
-                lastName: 'Gomez'
+                title: 'Avangers',
+                
             },
             {   
                 key: '2',
-                name: 'Juan',
-                lastName: 'Gomez'
+                title: 'Super Man',
+                
             },
             {   
                 key: '3',
-                name: 'Diana',
-                lastName: 'Londoño'
+                title: 'Batman',
+                
             }
 
         ]
         return (<Layout title="Recomendados...">
                     <FlatList 
+                        ListEmptyComponent={()=>this.renderEmpty()}
                         data={list} 
-                        renderItem={ ({item}) => <Text>{item.name} {item.lastName}</Text> }
+                        renderItem={ ({item}) => <Text>{item.title}</Text> }
+                        ItemSeparatorComponent={()=>this.itemSeparator()}
                     />
                 </Layout>)
     }
