6599bc499988079d668ad04d60aa88c8060db395
Lisandro Gomez <draivenl@gmail.com>
2019-11-09 12:15:43 -0500

    adding react-native-video

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {
+  View,
   Text
 } from 'react-native';
 
@@ -8,6 +9,8 @@ import Header from './src/sections/components/Header';
 import SuggestionList from './src/videos/containers/SuggestionList';
 import CategoryList from './src/videos/containers/CategoryList';
 import API from "./utils/api";
+
+import Video from 'react-native-video'
 
 class App extends Component {
   state = {
@@ -31,6 +34,29 @@ class App extends Component {
     return(
       <Home>
         <Header/>
+        <View
+          style={{
+            flex: 1,
+            height: 100
+          }}
+        >
+          <Video
+            // source={{
+            //   uri: 'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4'
+            // }}
+            source={
+              require('./assets/bolos.mp4')
+            }
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              bottom: 0,
+              top: 0,
+            }}
+            resizeMode="contain"
+          />
+        </View>
         <Text>Buscador</Text>
         <Text>Categorias</Text>
         <CategoryList list={this.state.categoryList}/>
