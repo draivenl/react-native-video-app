07f8e63fdd3136392896511fa600ce9e95204d3f
Lisandro Gomez <draivenl@gmail.com>
2019-11-17 09:12:53 -0500

    Basic animations with Amimated component

--- src/screens/containers/movie.js
+++ src/screens/containers/movie.js
@@ -7,9 +7,14 @@ import Header from "../../sections/components/Header";
 import Close from "../../sections/components/close";
 import Details from "../../videos/components/details";
 
+import { Animated } from "react-native";
 
 
 class Movie extends Component {
+    state = {
+        opacity: new Animated.Value(0)
+    }
+
     closeVideo = () => {
         this.props.dispatch({
             type: 'SET_SELECTED_MOVIE',
@@ -19,21 +24,40 @@ class Movie extends Component {
         })
     }
 
+    componentDidMount() {
+        Animated.timing(
+            this.state.opacity,
+            {
+                toValue: 1,
+                duration: 1000
+            }
+        ).start()
+    }
+
     render() {
         return(
-            <MovieLayout>
-                <Header>
-                    <Close
-                        onPress={this.closeVideo}
-                    ></Close>
-                </Header>
-                <Player/>
-                <Details {...this.props.movie}/>
-            </MovieLayout>
+            <Animated.View
+                style={{
+                    flex: 1,
+                    opacity: this.state.opacity,
+                }}
+            >
+                <MovieLayout>
+                    <Header>
+                        <Close
+                            onPress={this.closeVideo}
+                        ></Close>
+                    </Header>
+                    <Player/>
+                    <Details {...this.props.movie}/>
+                </MovieLayout>
+
+            </Animated.View>
         )
     }   
 
 }
+
 
 const mapStateToProps = (state) => {
     return {
